9ed124962fd0760632d002bd15ae437cb1066514
Arpit Nagpal <arpit.nagpal08@outlook.com>
2019-05-14 23:51:15 +0530

    dashboard front-end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ForumComponent } from './forum/forum.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent
+  },
+  {
+    path: 'forum',
+    component: ForumComponent
   }
 ];
 
