b9953a167ffd97bd6de1cc9067a44c4714ccaf92
Arpit Nagpal <arpit.nagpal08@outlook.com>
2019-06-16 10:43:34 +0530

    Added dist folder

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan')
 const PORT = process.env.PORT || 3000;
+const path = require("path")
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(express.static(path.join(__dirname, 'dist/todos')));
 
 // ROUTES
 const users = require("./routes/api");
